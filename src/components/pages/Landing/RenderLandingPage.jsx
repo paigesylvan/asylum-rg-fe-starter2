4ff45c5e76bedd5ca35553c8c6983c048fe1c3c1
PaigeSylvan <paigesylvan@gmail.com>
2024-04-26 18:29:55 -0500

    task 1 wip

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -111,7 +111,15 @@ function RenderLandingPage(props) {
               </div>
           </div>
        
-       
+          <div className="read-more-btn">
+          <Button
+            type="default"
+            style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+            onClick={() => history.push('/graphs')}
+          >
+            Read More
+          </Button>
+        </div>
         
         
         <p onClick={() => scrollToTop()} className="back-to-top">
